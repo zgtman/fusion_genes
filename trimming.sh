355c2c74fbe91ba888a46907704c21bcce14a10a
bioxsys <bioinformatician.cz@gmail.com>
2019-12-08 18:45:57 +0100

    add_to_git.sh

--- trimming.sh
+++ trimming.sh
@@ -5,7 +5,6 @@ T="$(date +%s)"
 source fusion_genes/config
 
 mkdir qc_filtered
-mkdir fastq_filtered
 
 number=$(ls *.fastq.gz | rev | cut -c 22- | rev | sort | uniq | wc -l)
 
@@ -18,7 +17,7 @@ echo "INFO: Found number of files: $number"
 echo "INFO: Analyzing file: $i"
 
 
-fastp -i ${i}_L001_R1_001.fastq.gz -I ${i}_L001_R2_001.fastq.gz -o ${i}_trim_L001_R1_001.fastq.gz -O ${i}_trim_L001_R2_001.fastq.gz -j ${i}.json -h ${i}.html -t $cpu -M 18
+fastp -i ${i}_L001_R1_001.fastq.gz -I ${i}_L001_R2_001.fastq.gz -o ${i}_trim_L001_R1_001.fastq.gz -O ${i}_trim_L001_R2_001.fastq.gz -j ${i}.json -h ${i}.html -t $cpu
 
 done;
 
@@ -33,7 +32,7 @@ if [ `ls -1 *trim*fastq.gz 2>/dev/null | wc -l ` -gt 0 ];
 
 then 
 
-echo "INFO" Trimming was performed. Creating original_fastq folder and moving original FASTQ files there"
+echo "INFO: Trimming was performed. Creating original_fastq folder and moving original FASTQ files there"
 
 
 fi
