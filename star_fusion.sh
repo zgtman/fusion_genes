bf5109ebd4153abe37bda05f445651afbfc4054a
bioxsys <bioinformatician.cz@gmail.com>
2019-12-09 21:10:03 +0100

    add_to_git.sh

--- star_fusion.sh
+++ star_fusion.sh
@@ -23,25 +23,27 @@ STAR \
 	--readFilesIn ${i}_L001_R1_001.fastq.gz ${i}_L001_R2_001.fastq.gz \
 	--readFilesCommand "gunzip -c" \
 	--twopassMode Basic \
-	--outSAMstrandField intronMotif --outBAMcompression 0 --outSAMtype BAM \
+	--outStd BAM_Unsorted --outSAMtype BAM Unsorted \
+	--outSAMunmapped Within \
+	--outBAMcompression 0 \
 	--outFilterMultimapNmax 1 --outFilterMismatchNmax 3 \
 	--outFileNamePrefix ${i} \
 	--chimSegmentMin 10 \
-	--alignMatesGapMax 100000 --alignIntronMax 100000 \
-    --alignSJDBoverhangMin 10 \
+	--alignMatesGapMax 100000 \
+	--alignIntronMax 100000 \
 	--chimOutType WithinBAM SoftClip \
-    --chimMultimapScoreRange 3 \
-    --chimMultimapNmax 20 \
-    --outSAMattrRGline ID:GRPundef \
 	--chimJunctionOverhangMin 10 \
 	--chimScoreMin 1 \
-	--chimOutJunctionFormat 1 --chimScoreJunctionNonGTAG 0 \
+	--chimOutJunctionFormat 1 \
+	--chimScoreDropMax 30 \
+	--chimScoreJunctionNonGTAG 0 \
 	--chimScoreSeparation 1 \
 	--alignSJstitchMismatchNmax 5 -1 5 5 \
 	--chimNonchimScoreDropMin 10 \
 	--peOverlapNbasesMin 12 \
 	--peOverlapMMp 0.1 \
-	--chimSegmentReadGapMax 3
+   	--chimOutJunctionFormat 1 --chimScoreJunctionNonGTAG 0 \
+	--chimSegmentReadGapMax 3 > ${i}_out.bam
 
 
-done;
+done
