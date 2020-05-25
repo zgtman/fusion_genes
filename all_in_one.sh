d9c4fa5314a8911e86b56e8dd8be416cdb9205dc
zgtman <bioinformatician.cz@gmail.com>
2020-05-25 10:53:38 +0200

    add_to_git.sh

--- all_in_one.sh
+++ all_in_one.sh
@@ -20,13 +20,13 @@ ln -s fusion_genes/annotation.sh
 
 ### RUN SCRIPT
 
-#bash fastqc_check.sh
-#bash trimming.sh
-#bash star_align.sh ## align only for arriba
-#bash arriba.sh
-#bash arriba_plot_result.sh
+bash fastqc_check.sh
+bash trimming.sh
+bash star_align.sh ## align only for arriba
+bash arriba.sh
+bash arriba_plot_result.sh
 #bash star_fusion.sh # not used
-#bash star_fusion_all.sh
+bash star_fusion_all.sh
 bash variant_calling_VS.sh
 bash annotation.sh
 
